07966ab9f3ee9cac37009281c5a53ca43e8b31ab
Hugo Ganet <ganethugo@gmail.com>
2025-04-26 18:09:47 +0200

    add check for simumlation stop throughout philo_life

--- src/core/routine.c
+++ src/core/routine.c
@@ -6,7 +6,7 @@
 /*   By: hugoganet <hugoganet@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 17:49:48 by hugoganet         #+#    #+#             */
-/*   Updated: 2025/04/26 17:38:38 by hugoganet        ###   ########.fr       */
+/*   Updated: 2025/04/26 18:04:28 by hugoganet        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,37 +33,48 @@ static void log_action(t_philo *philo, const char *msg)
 
 void *philo_life(void *arg)
 {
-	t_philo *philo = (t_philo *)arg;
+	t_philo *philo;
 
+	philo = (t_philo *)arg;
 	if (philo->id % 2 == 0)
 		usleep(500);
-
 	while (!is_simulation_stopped(philo->config))
 	{
+		if (is_simulation_stopped(philo->config))
+			break;
 		pthread_mutex_lock(philo->left_fork);
 		log_action(philo, "has taken a fork");
 
-		if (pthread_mutex_lock(philo->right_fork) != 0)
+		if (is_simulation_stopped(philo->config))
 		{
 			pthread_mutex_unlock(philo->left_fork);
-			continue;
+			break;
 		}
+		pthread_mutex_lock(philo->right_fork);
 		log_action(philo, "has taken a fork");
 
-		// EAT
-		philo->last_meal = get_timestamp_ms();
+		if (is_simulation_stopped(philo->config))
+		{
+			pthread_mutex_unlock(philo->left_fork);
+			pthread_mutex_unlock(philo->right_fork);
+			break;
+		}
 		log_action(philo, "is eating");
+		philo->last_meal = get_timestamp_ms();
 		safe_sleep(philo->config, philo->config->time_to_eat);
 		philo->meals_eaten++;
 
 		pthread_mutex_unlock(philo->right_fork);
 		pthread_mutex_unlock(philo->left_fork);
 
+		if (is_simulation_stopped(philo->config))
+			break;
 		log_action(philo, "is sleeping");
 		safe_sleep(philo->config, philo->config->time_to_sleep);
 
+		if (is_simulation_stopped(philo->config))
+			break;
 		log_action(philo, "is thinking");
 	}
-
 	return (NULL);
 }
