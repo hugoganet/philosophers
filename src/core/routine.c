79236e0722ec1b461018c7897a912d37dfd1a83b
Hugo Ganet <ganethugo@gmail.com>
2025-04-27 08:39:48 +0200

    add special logic if just one philo

--- src/core/routine.c
+++ src/core/routine.c
@@ -6,7 +6,7 @@
 /*   By: hugoganet <hugoganet@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 17:49:48 by hugoganet         #+#    #+#             */
-/*   Updated: 2025/04/26 18:33:41 by hugoganet        ###   ########.fr       */
+/*   Updated: 2025/04/26 18:34:53 by hugoganet        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,15 @@ void *philo_life(void *arg)
 	t_philo *philo;
 
 	philo = (t_philo *)arg;
-	
+	if (philo->config->nb_philo == 1)
+	{
+		pthread_mutex_lock(philo->left_fork);
+		log_action(philo, "has taken a fork");
+		while (!is_simulation_stopped(philo->config))
+			usleep(1000);
+		pthread_mutex_unlock(philo->left_fork);
+		return (NULL);
+	}
 	while (!is_simulation_stopped(philo->config))
 	{
 		take_forks(philo);
